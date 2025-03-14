28930c1d3493a51522af4b2a545af49c5d04f47d
Gui898 <gui.miojo.lasanha@gmail.com>
2025-03-14 19:10:47 -0300

    Initial Window (pre-version)

---
+++ InitialFrame.java
@@ -0,0 +1,73 @@
+package Chess;
+
+import javax.sound.sampled.AudioInputStream;
+import javax.sound.sampled.AudioSystem;
+import javax.sound.sampled.Clip;
+import javax.sound.sampled.FloatControl;
+import javax.swing.*;
+import java.awt.*;
+import java.io.File;
+
+public class InitialFrame extends JFrame {
+
+    private static final Dimension screenSize = Toolkit.getDefaultToolkit().getScreenSize();
+    public static String path = System.getProperty("user.dir") + "\\Sounds\\";
+    public static Clip clipIs;
+
+    static {
+        try {
+            clipIs = createClip(path + "InitialST.wav");
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    public void initialWindow() throws Exception {
+
+        FloatControl volumeControlSt = (FloatControl) clipIs.getControl(FloatControl.Type.MASTER_GAIN);
+        volumeControlSt.setValue(-25.0f);
+        clipIs.open();
+        clipIs.start();
+
+        Font customFont = Font.createFont(Font.TRUETYPE_FONT, new File(System.getProperty("user.dir")+ "\\Fonts\\DancingScript-Bold.ttf"));
+        customFont = customFont.deriveFont(Font.PLAIN, 130);
+
+        setLayout(null);
+        setResizable(false);
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setExtendedState(JFrame.MAXIMIZED_BOTH);
+        setSize(screenSize.width, screenSize.height);
+        setFont(customFont);
+
+        ImageIcon imageIcon = new ImageIcon(System.getProperty("user.dir") + "/Images/ChessIcon.jpg");
+        setIconImage(imageIcon.getImage());
+
+        ImageIcon backgroundImage = new ImageIcon(System.getProperty("user.dir") + "/Images/ChessInitialImage.jpg");
+        JLabel background = new JLabel("", backgroundImage, JLabel.CENTER);
+        background.setBounds(0, 0, screenSize.width, screenSize.height);
+
+        add(background);
+
+        JLabel textArea = new JLabel("Chess");
+        textArea.setBounds(100, 60, 400, 150);
+        textArea.setForeground(Color.WHITE);
+        textArea.setBackground(Color.BLACK);
+        textArea.setFont(customFont);
+        textArea.setOpaque(true);
+        textArea.setHorizontalAlignment(SwingConstants.CENTER);
+        textArea.setVerticalAlignment(SwingConstants.CENTER);
+
+        background.add(textArea);
+
+        add(new InitialFrameButton(this));
+
+        setVisible(true);
+    }
+
+    public static Clip createClip(String path) throws Exception {
+        AudioInputStream audioStream = AudioSystem.getAudioInputStream(new File(path));
+        Clip clip = AudioSystem.getClip();
+        clip.open(audioStream);
+        return clip;
+    }
+}
--- Main.java
+++ Main.java
@@ -1,57 +1,12 @@
 package Chess;
 
-import javax.sound.sampled.*;
-import java.io.File;
-import java.io.IOException;
-
 public class Main {
 
     public static void main(String[] args) throws Exception {
 
-        SuperButton.board.openBoard();
+        InitialFrame a = new InitialFrame();
+        a.initialWindow();
 
-        Thread soundThread = new Thread(() -> {
-            try {
-                String path = System.getProperty("user.dir") + "\\Sounds\\";
 
-                Clip clipSt = createClip(path + "SoundTrack.wav");
-                Clip clipGr = createClip(path + "GoodResult.wav");
-                Clip clipVs = createClip(path + "VictorySound.wav");
-
-                // Fixing the Volume
-                FloatControl volumeControlSt = (FloatControl) clipSt.getControl(FloatControl.Type.MASTER_GAIN);
-                FloatControl volumeControlGr = (FloatControl) clipGr.getControl(FloatControl.Type.MASTER_GAIN);
-                FloatControl volumeControlVs = (FloatControl) clipVs.getControl(FloatControl.Type.MASTER_GAIN);
-                float volume = -25.0f;
-                volumeControlSt.setValue(volume);
-                volumeControlGr.setValue(volume);
-                volumeControlVs.setValue(volume);
-
-                clipSt.loop(Clip.LOOP_CONTINUOUSLY);
-                clipSt.start();
-
-                while (!GraphicalBoard.endGame) {
-                    Thread.sleep(100);
-                }
-
-                clipSt.stop();
-                clipGr.start();
-                clipVs.start();
-
-            } catch (Exception e) {
-                e.printStackTrace();
-            }
-        });
-
-        // Start
-        soundThread.start();
-    }
-
-    // Método auxiliar pra criar um Clip
-    public static Clip createClip(String path) throws Exception {
-        AudioInputStream audioStream = AudioSystem.getAudioInputStream(new File(path));
-        Clip clip = AudioSystem.getClip();
-        clip.open(audioStream);
-        return clip;
     }
 }
